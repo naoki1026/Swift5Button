49d251409f6e6f12911c4413b32fa1e7cf4f982d
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-02-15 16:32:07 +0900

    Swift5ButtonをGitHubにアップする

---
+++ Swift5Button/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5Button
+//
+//  Created by Naoki Arakawa on 2019/02/15.
+//  Copyright © 2019 Naoki Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
